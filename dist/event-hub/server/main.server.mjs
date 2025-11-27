a573ba2105b5dc433d4e9e3f0d7966fc15c65093
Aarti <119279339+aartiwamane@users.noreply.github.com>
2025-11-27 12:49:33 +0530

    Add built Angular dist for Render

---
+++ dist/event-hub/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-KZEWPDJF.mjs";import"./chunk-IVW7MF3D.mjs";import"./chunk-DCFYTP2A.mjs";export{a as default};
---
+++ dist/eventshub/server/render-utils.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{Pa as e,tb as o,ub as r,vb as n}from"./chunk-IVW7MF3D.mjs";import"./chunk-DCFYTP2A.mjs";export{n as renderApplication,r as renderModule,e as \u0275Console,o as \u0275SERVER_CONTEXT};
