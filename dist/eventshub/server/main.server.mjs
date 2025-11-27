cf2dfb1a0a58d14c3dfdca299f2957e748cc129e
Aarti <119279339+aartiwamane@users.noreply.github.com>
2025-11-27 14:18:23 +0530

    fixed API URLs for production

--- dist/eventshub/server/main.server.mjs
+++ dist/eventshub/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-KZEWPDJF.mjs";import"./chunk-IVW7MF3D.mjs";import"./chunk-DCFYTP2A.mjs";export{a as default};
+import{a}from"./chunk-DKZ2IGBQ.mjs";import"./chunk-IVW7MF3D.mjs";import"./chunk-DCFYTP2A.mjs";export{a as default};
